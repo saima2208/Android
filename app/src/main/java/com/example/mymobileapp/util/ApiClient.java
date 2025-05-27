ae5a673cace8b32ad795d7761d2c6a17e85b0c23
saima <saimasonya22@gmail.com>
2025-05-27 18:30:50 +0600

    update delete done

--- app/src/main/java/com/example/mymobileapp/util/ApiClient.java
+++ app/src/main/java/com/example/mymobileapp/util/ApiClient.java
@@ -6,7 +6,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://192.168.0.8:8081/";
+    private static final String BASE_URL = "http://192.168.100.2:8081/";
 
     private static Retrofit retrofit;
 
